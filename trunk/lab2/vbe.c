850459eb4e9922fa43e63be332c9b45811815442
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-09-28 13:44:17 +0000

    Merged some code.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@8 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- trunk/lab2/vbe.c
+++ trunk/lab2/vbe.c
@@ -5,10 +5,10 @@
 #include "vbe.h"
 #include "lmlib.h"
 
-#include <stdio.h>
-
 #define LINEAR_MODEL_BIT 14
 #define BIT(n) (0x1 << (n))
+
+#define VBE_MODE_INFO_BLOCK_SIZE sizeof(vbe_mode_info_t)
 
 #define VBE_MODE 0x4F
 #define RET_VBE_MODE_INFO 0x01
@@ -17,12 +17,12 @@
 #define PB2BASE(x) (((x) >> 4) & 0x0F000)
 #define PB2OFF(x) ((x) & 0x0FFFF)
 
-int vbe_get_mode_info(unsigned short mode, vbe_mode_info_t* vmi_p)
-{
-    struct reg86u r;
-    mmap_t map;
+int vbe_get_mode_info(unsigned short mode, vbe_mode_info_t *vmi_p) {
 
-    if (mode < 0x100)
+	struct reg86u r;
+	mmap_t map;
+	
+	if (mode < 0x100)
     {
         printf("lab2/vbe_get_mode_info: mode is non-VBE (%u)", mode);
         return -1;
@@ -33,33 +33,27 @@ int vbe_get_mode_info(unsigned short mode, vbe_mode_info_t* vmi_p)
         printf("lab2/vbe_get_mode_info: failed in lm_init");
         return -1;
     }
-
-    lm_alloc(sizeof(vbe_mode_info_t), &map);
-
-    /*
-    if (!vmi_temp)
-    {
-        printf("lab2/vbe_get_mode_info: failed in lm_alloc");
-        return -1;
-    }
-    */
-
-    r.u.b.ah = VBE_MODE;
-    r.u.b.al = RET_VBE_MODE_INFO;
-    r.u.w.es = PB2BASE(map.phys);
-    r.u.w.di = PB2OFF(map.phys);
-    r.u.w.cx = mode | BIT(LINEAR_MODEL_BIT);
-    r.u.b.intno = BIOS_VIDEO_SERVICE;
-
-    if (sys_int86(&r))
+	
+	
+	lm_alloc(VBE_MODE_INFO_BLOCK_SIZE, &map);
+	
+	r.u.b.ah = VBE_MODE;
+	r.u.b.al = RET_VBE_MODE_INFO;
+	r.u.w.es = PB2BASE(map.phys);
+	r.u.w.di = PB2OFF(map.phys);
+	r.u.w.cx = mode | BIT(LINEAR_MODEL_BIT);;
+	r.u.b.intno = BIOS_VIDEO_SERVICE;
+	
+	if (sys_int86(&r))
     {
         printf("lab2/vbe_get_mode_info: failed in sys_int86");
         return -1;
     }
-
-    *vmi_p = *(vbe_mode_info_t*)map.virtual; /* NEEDS to be called after sys_int86 because reasons */
-
-    lm_free(&map);
-
-    return r.u.w.ax; /* VBE return status */
+	
+	*vmi_p = *(vbe_mode_info_t*)map.virtual; /* NEEDS to be called after sys_int86 because reasons */
+	
+	lm_free(&map);
+	
+	return r.u.w.ax; /* VBE return status */
 }
+
--- trunk/lab2/video_gr.c
+++ trunk/lab2/video_gr.c
@@ -4,7 +4,6 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 
-#include "lmlib.h"
 #include "vbe.h"
 
 /* Constants for VBE 0x105 mode */
@@ -14,8 +13,7 @@
 #define V_RES		  768
 #define BITS_PER_PIXEL	  8
 
-#define LINEAR_MODEL_BIT 14
-#define BIT(n) (0x1 << (n))
+#define min(x,y) ((x) < (y) : (x) ? (y))
 
 #define VBE_MODE 0x4F
 #define SET_VBE_MODE 0x02
@@ -25,24 +23,24 @@
 
 /* Private global variables */
 
-static char *video_mem;		/* Process address to which VRAM is mapped */
+static char *video_mem; /* Process address to which VRAM is mapped */
 
-static unsigned h_res;		/* Horizontal screen resolution in pixels */
-static unsigned v_res;		/* Vertical screen resolution in pixels */
+static unsigned h_res; /* Horizontal screen resolution in pixels */
+static unsigned v_res; /* Vertical screen resolution in pixels */
 static unsigned bits_per_pixel; /* Number of VRAM bits per pixel */
 
-void* vg_init(unsigned long mode)
-{
-    vbe_mode_info_t vmi_p;
-    struct reg86u r;
-    struct mem_range mr;
-    unsigned int vram_size;
+void * vg_init(unsigned long mode) {
+	unsigned int vram_size;
+	struct mem_range mr;
+	vbe_mode_info_t vmi_p;
+	struct reg86u r;
 
-    r.u.b.intno = BIOS_VIDEO_SERVICE;
+	r.u.b.intno = BIOS_VIDEO_SERVICE;
     r.u.b.ah = VBE_MODE;
     r.u.b.al = SET_VBE_MODE;
     r.u.w.bx = BIT(LINEAR_MODEL_BIT) | mode;
-    if (sys_int86(&r))
+
+	if (sys_int86(&r))
     {
         printf("lab2/vg_init: failed in sys_int86");
         return NULL;
@@ -54,13 +52,13 @@ void* vg_init(unsigned long mode)
         return NULL;
     }
 
-    h_res = vmi_p.XResolution;
-    v_res = vmi_p.YResolution;
-    bits_per_pixel = vmi_p.BitsPerPixel;
+	h_res = vmi_p.XResolution;
+	v_res = vmi_p.YResolution;
+	bits_per_pixel = vmi_p.BitsPerPixel;
 
-    vram_size =  h_res * v_res * (bits_per_pixel / 8);
-
-    mr.mr_base = vmi_p.PhysBasePtr;
+	vram_size =  h_res * v_res * (bits_per_pixel / 8);
+	
+	mr.mr_base = vmi_p.PhysBasePtr;
     mr.mr_limit = mr.mr_base + vram_size;
     if (sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr))
     {
@@ -68,7 +66,7 @@ void* vg_init(unsigned long mode)
         return NULL;
     }
 
-    video_mem = vm_map_phys(SELF, (void*)mr.mr_base, vram_size);
+	video_mem = vm_map_phys(SELF, (void*)mr.mr_base, vram_size);
 
     if (video_mem == MAP_FAILED)
     {
@@ -79,8 +77,7 @@ void* vg_init(unsigned long mode)
     return video_mem;
 }
 
-int vg_fill(unsigned long color)
-{
+int vg_fill(unsigned long color) {
     int i, j;
     unsigned long colorTemp;
     char* ptr;
@@ -104,36 +101,118 @@ int vg_fill(unsigned long color)
 int vg_set_pixel(unsigned long x, unsigned long y, unsigned long color) {
 	int i;
 	char* vptr;
+	
+	if (x < 0 || x > h_res || y < 0 || y > v_res) {
+		return -1;
+	}
+	
 	i = (y * h_res + x) * (bits_per_pixel / 8);
 	vptr = &video_mem[i];
 	for (i = 0; i < bits_per_pixel / 8; i++) {
 		*vptr = (char) color;
 		vptr++;
-		color >> 8;
+		color >>= 8;
 	}
 	return 0;
 }
 
 long vg_get_pixel(unsigned long x, unsigned long y) {
+	int i;
+	long res;
+	char* vptr;
+	
+	if (x < 0 || x > h_res || y < 0 || y > v_res) {
+		return ERR;
+	}
+	
+	i = (y * h_res + x) * (bits_per_pixel / 8);
+	vptr = &video_mem[i];
+	for (i = 0; i < bits_per_pixel / 8; i++) {
+		res <<= 8;
+		res |= *vptr;
+		vptr++;
+	}
+	return res;
+}
+
+int vg_draw_line(unsigned long xi, unsigned long yi, unsigned long xf,
+		unsigned long yf, unsigned long color) {
+	long i;
+	long m;
+
+	if (xi < 0 || xi > h_res || yi < 0 || yi > v_res || xf < 0 || xf > h_res || yf < 0 || yf > v_res) {
+		return ERR;
+	}
+	
+	if (xi == xf) {
+		if (yi > yf) {
+			for (i = yi; i > yf; i--)
+				vg_set_pixel(xi, i, color);
+		} else {
+			for (i = yi; i < yf; i++)
+				vg_set_pixel(xi, i, color);
+		}
+
+	} else if (xi < xf) {
+
+		m = (yf - yi) / (xf - xi);
+		for (i = xi; i < xf; i++) {
+			vg_set_pixel(i, yi, color);
+			yi += m;
+		}
+
+	} else { /* xi > xf */
+
+		m = (yi - yf) / (xi - xf);
+		for (i = xi; i > xf; i--) {
+			vg_set_pixel(i, yi, color);
+			yi += m;
+		}
+
+	}
+
 	return 0;
 }
 
-int vg_draw_line(unsigned long xi, unsigned long yi, 
-		 unsigned long xf, unsigned long yf, unsigned long color) {
+int vg_draw_rectangle(unsigned long x1, unsigned long y1, unsigned long x2,
+		unsigned long y2, unsigned long color) {
+	int x, y;
+
+	if (x1 < 0 || x1 > h_res || y1 < 0 || y1 > v_res || x2 < 0 || x2 > h_res || y2 < 0 || y2 > v_res) {
+		return ERR;
+	}
+	
+	if (x1 < x2 && y1 < y2)
+		for (x = x1; x < x2; x++)
+			for (y = y1; y < y2; y++)
+				vg_set_pixel(x, y, color);
+	else if (x2 < x1 && y1 < y2)
+		for (x = x2; x < x1; x++)
+			for (y = y1; y < y2; y++)
+				vg_set_pixel(x, y, color);
+	else if (x2 < x1 && y2 < y1)
+		for (x = x2; x < x1; x++)
+			for (y = y2; y < y1; y++)
+				vg_set_pixel(x, y, color);
+	else if (x1 < x2 && y2 < y1)
+		for (x = x1; x < x2; x++)
+			for (y = y2; y < y1; y++)
+				vg_set_pixel(x, y, color);
+
 	return 0;
 }
 
 int vg_exit() {
-  struct reg86u reg86;
+	struct reg86u reg86;
 
-  reg86.u.b.intno = 0x10; /* BIOS video services */
+	reg86.u.b.intno = 0x10; /* BIOS video services */
 
-  reg86.u.b.ah = 0x00;    /* Set Video Mode function */
-  reg86.u.b.al = 0x03;    /* 80x25 text mode*/
+	reg86.u.b.ah = 0x00; /* Set Video Mode function */
+	reg86.u.b.al = 0x03; /* 80x25 text mode*/
 
-  if( sys_int86(&reg86) != OK ) {
-      printf("\tvg_exit(): sys_int86() failed \n");
-      return 1;
-  } else
-      return 0;
+	if (sys_int86(&reg86) != OK) {
+		printf("\tvg_exit(): sys_int86() failed \n");
+		return 1;
+	} else
+		return 0;
 }
