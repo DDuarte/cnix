aa438ed8f47f80d978204b88d6793f55f157abc3
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-10-02 12:50:31 +0000

    lab3: Implemented some functions.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@29 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- trunk/lab3/timer.c
+++ trunk/lab3/timer.c
@@ -1,7 +1,13 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
+#include <minix/com.h>
 
 #include "i8254.h"
+
+/*#define BIT(n) (0x1 << (n))*/
+
+static int timer_hook_id;
+
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
     /* change Timer timer to mode 3 */
@@ -18,16 +24,22 @@ int timer_set_square(unsigned long timer, unsigned long freq) {
 
 int timer_subscribe_int(void ) {
 
-    return 1;
+    sys_irqsetpolicy(TIMER0_IRQ, IRQ_ENABLE, &timer_hook_id);
+    sys_irqenable(&timer_hook_id);
+    
+	return 0;
 }
 
 int timer_unsubscribe_int() {
 
-    return 1;
+    sys_irqdisable(&timer_hook_id);
+    /*sys_irqmpolicy(&timer_hook_id);*/
+    
+	return 0;
 }
 
 void timer_int_handler() {
-
+    printf("Hello!\n");
 }
 
 int timer_test_square(unsigned long freq) {
@@ -36,6 +48,35 @@ int timer_test_square(unsigned long freq) {
 }
 
 int timer_test_int(unsigned long time) {
-
-    return 1;
+    
+    message msg;
+    int ipc_status;
+    int r, cnt = 0;
+    
+    timer_subscribe_int();
+    
+    while( cnt <= time ) {
+        r =  driver_receive(ANY, &msg, &ipc_status); /* Get a request message. */
+        if ( r != 0 ) { 
+            printf("driver_receive failed with: %d", r);
+            continue;
+        }
+        if (is_ipc_notify(ipc_status)) { /* received notification */
+            switch (_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE: /* hardware interrupt notification */				
+                    if (msg.NOTIFY_ARG & BIT(timer_hook_id)) { /* subscribed interrupt */
+                        timer_int_handler();
+                        cnt++;
+                    }
+                    break;
+                default:
+                    break; /* no other notifications expected: do nothing */	
+            }
+        } else { /* received a standard message, not a notification */
+            /* no standard messages expected: do nothing */
+        }
+    }
+    
+    timer_unsubscribe_int();
+    return 0;
 }
