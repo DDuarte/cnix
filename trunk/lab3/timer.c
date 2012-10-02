aeb13393e2fadab465b6ea0ba7a9cf47968a0326
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-10-02 11:45:56 +0000

    lab3 : implemented timer_set_square() and timer_test_square() functions in timer.c. lab3 : created main in order to test refereed functions.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@25 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- trunk/lab3/timer.c
+++ trunk/lab3/timer.c
@@ -1,9 +1,19 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
 
-int timer_set_square(unsigned long timer, unsigned long freq) {
+#include "i8254.h"
 
-	return 1;
+int timer_set_square(unsigned long timer, unsigned long freq) {
+	/* change Timer timer to mode 3 */
+	if (timer > 2)
+		return 1;
+		
+	sys_outb(TIMER_CTRL, TIMER_BIN | TIMER_SQR_WAVE | TIMER_LSB_MSB | timer << 6);
+	
+	sys_outb(TIMER_0, (TIMER_FREQ / freq) & 0xFF); /* LSB */
+	sys_outb(TIMER_0, (TIMER_FREQ / freq) >> 8); /* MSB */
+	
+	return 0;
 }
 
 int timer_subscribe_int(void ) {
@@ -21,7 +31,7 @@ void timer_int_handler() {
 }
 
 int timer_test_square(unsigned long freq) {
-	
+	timer_set_square(0, freq);
 	return 1;
 }
 
