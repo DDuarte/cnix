f41a3d204a9b76345a13f7291ae3e0ce07b72306
ei11101 <ei11101@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-10-03 21:50:39 +0000

    lab3: Implement speaker_ctrl and speaker_test
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@31 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- trunk/lab3/speaker.c
+++ trunk/lab3/speaker.c
@@ -1,13 +1,41 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
 
+#include "i8254.h"
+
 int speaker_ctrl(unsigned char on) {
 
-    return 1;
+    if (on == 1) {
+        sys_outb(SPEAKER_CTRL, 0x3);
+    } else {
+        sys_outb(SPEAKER_CTRL, 0x0);
+    }
+
+    return 0;
 }
 
 int speaker_test(unsigned long freq, unsigned long time) {
 
-    return 1;
+    if (speaker_ctrl(1) != 0) {
+        printf("lab3/speaker_test: speaker_ctrl(1) failed\n");
+        return 1;
+    }
+
+    if (timer_set_square(2, freq) != 0) {
+        printf("lab3/speaker_test: timer_set_square failed\n");
+        return 1;
+    }
+
+    if (timer_test_int(time * 60) != 0) {
+        printf("lab3/speaker_test: timer_test_int failed\n");
+        return 1;
+    }
+
+    if (speaker_ctrl(0) != 0) {
+        printf("lab3/speaker_test: speaker_ctrl(0) failed\n");
+        return 1;
+    }
+
+    return 0;
 }
 
