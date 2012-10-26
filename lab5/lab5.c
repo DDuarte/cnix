f8902599884ce3c127ac45fd9efa55b788673911
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-10-26 17:42:10 +0000

    lab5: implement test_asynch and test_config functions.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@55 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -2,5 +2,5 @@
 
 int main(int argc, char *argv[]) {
     sef_startup();
-    return test_packet();
+    return test_config();
 }
--- lab5/test5.c
+++ lab5/test5.c
@@ -11,9 +11,11 @@
 static unsigned char packet[3] = { 0, 0, 0 };
 static short counter = 0;
 static int num_interrupts = 0;
-static unsigned int init = 0;
 
-static void mouseCallback() {
+static short timerDuration = 0;
+static long timerCounter = 0;
+
+static void _mouseCallback() {
 
     unsigned long datatemp;
     if (sys_inb(DATA_REG, &datatemp) != 0) {
@@ -29,8 +31,6 @@ static void mouseCallback() {
     counter = (counter + 1) % 3;
     num_interrupts++;
     
-    if (num_interrupts == 300) int_stop_handler();
-    
     if (counter == 0) {
         printf("B1=0x%X B2=0x%X B3=0x%X LB=%d MB=%d RB=%d XOV=%d YOV=%d X=%d Y=%d\n", packet[0], packet[1], packet[2], 
         !!bit_isset(packet[0], 0),
@@ -43,19 +43,28 @@ static void mouseCallback() {
     }
 }
 
+static void mouseCallback(void) {
+    _mouseCallback();
+    if (num_interrupts == 300) int_stop_handler();
+}
+
+static void mouseAsynchCallback(void) {
+    _mouseCallback();
+}
+
+static void timer0Callback(void) {
+    timerCounter++;
+    if (timerCounter / 60 == timerDuration)
+        int_stop_handler();
+}
+
 int test_packet() {
     int mouseInterrupt;
     unsigned long res;
     
     int_init();
     
-    write_kbc(CMD_REG, 0xD4) ; /* Write Byte to Mouse */    
-    write_kbc(DATA_REG, 0xF5); /* Enable Sending Data Packets */
-    
-    if (sys_inb(DATA_REG, &res) != 0) {
-        printf("read_kbc: sys_inb (2) failed.\n");
-    }
-    printf("DEBUG: acknowledge1: 0x%X\n", res);
+
     
     write_kbc(CMD_REG, 0xD4) ; /* Write Byte to Mouse */    
     write_kbc(DATA_REG, 0xF4); /* Enable Sending Data Packets */
@@ -63,7 +72,8 @@ int test_packet() {
     if (sys_inb(DATA_REG, &res) != 0) {
         printf("read_kbc: sys_inb (2) failed.\n");
     }
-    printf("DEBUG: acknowledge2: 0x%X\n", res);
+    printf("DEBUG: acknowledge: 0x%X\n", res);
+   
     
     printf("DEBUG: Subscribing interruption\n");
     mouseInterrupt = int_subscribe(12, IRQ_REENABLE | IRQ_EXCLUSIVE, &mouseCallback);
@@ -79,8 +89,82 @@ int test_packet() {
 }
 
 int test_asynch(unsigned short duration) {
-    /* To be completed ... */
+    int mouseInterrupt, timer0Interrupt;
+    unsigned long res;
+    
+    timerDuration = duration;
+    
+    int_init();
+
+    write_kbc(CMD_REG, 0xD4) ; /* Write Byte to Mouse */    
+    write_kbc(DATA_REG, 0xF4); /* Enable Sending Data Packets */
+    
+    if (sys_inb(DATA_REG, &res) != 0) {
+        printf("read_kbc: sys_inb (2) failed.\n");
+    }
+    printf("DEBUG: acknowledge: 0x%X\n", res);
+   
+    
+    printf("DEBUG: Subscribing mouse interruption\n");
+    mouseInterrupt = int_subscribe(MOUSE_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &mouseAsynchCallback);
+    printf("DEBUG: mouseInterrupt: %d\n", mouseInterrupt);
+    
+    printf("DEBUG: Subscribing Timer0 interruption\n");
+    mouseInterrupt = int_subscribe(TIMER0_IRQ, IRQ_REENABLE, &timer0Callback);
+    printf("DEBUG: timer0Interrupt: %d\n", timer0Interrupt);
+    
+    printf("DEBUG: STARTING_HANDLER.\n");
+    
+    
+    
+    res = int_start_handler();
+    
+    int_unsubscribe(mouseInterrupt);
+    int_unsubscribe(timer0Interrupt);
+    
+    return res;
 }
 int test_config(void) {
-    /* To be completed ... */
+    unsigned long res;
+    unsigned char byte[3];
+
+    write_kbc(CMD_REG, 0xD4) ; /* Write Byte to Mouse */    
+    write_kbc(DATA_REG, 0xE9); /* Enable Sending Data Packets */
+    
+    if (sys_inb(DATA_REG, &res) != 0) {
+        printf("read_kbc: sys_inb (2) failed.\n");
+    }
+    
+    
+    while (1) {
+        sys_inb(DATA_REG, &res);
+        if (!bit_isset(res, 7) && !bit_isset(res, 3))
+            break;
+    }
+    byte[0] = res;
+    
+    while (1) {
+        sys_inb(DATA_REG, &res);
+        if (res == 0x00 || res == 0x01 || res == 0x02 || res == 0x03)
+            break;
+    }
+    byte[1] = res;
+   
+    tickdelay(micros_to_ticks(50000));
+    
+    sys_inb(DATA_REG, &res);
+    byte[2] = res;
+    
+    printf("Mode: %s\nEnable: %d\nScaling: %s\nLB: %d\nMB: %d\nRB: %d\nResolution: %d count/mm\nSample Rate: %d\n",
+            (!!bit_isset(byte[0], 6) ? "Remote" : "Stream"),
+            (!!bit_isset(byte[0], 5)),
+            (!!bit_isset(byte[0], 4) ? "1:1" : "2:1"),
+            (!!bit_isset(byte[0], 3)),
+            (!!bit_isset(byte[0], 2)),
+            (!!bit_isset(byte[0], 1)),
+            0x1 << byte[1],
+            byte[2]);
+            
+    return 0;
 }
+
