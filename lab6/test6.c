ebd75342df74018cff36580563fa152a037ea02a
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-10-31 11:17:30 +0000

    lab6: Create lab6 folder and add needed files.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@65 dd1ae04f-8964-4e22-877d-0b2b87089ac8

---
+++ lab6/test6.c
@@ -0,0 +1,12 @@
+int test_conf(void) {
+	/* To be completed */
+}
+
+int test_date(void) {
+	/* To be completed */
+}
+
+int test_int(/* to be defined in class */) { 
+	/* To be completed */
+}
+
---
+++ lab6/test6.h
@@ -0,0 +1,4 @@
+int test_conf(void);
+int test_date(void);
+int test_period(/* to be defined in class */);
+
