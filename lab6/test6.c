b1c05177e248b0d4d56c1ca5e85989997fa35c8c
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-11-05 17:36:41 +0000

    lab6: Implement test_date() function.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@69 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- lab6/test6.c
+++ lab6/test6.c
@@ -2,7 +2,10 @@
 #include "interrupt.h"
 #include "debug.h"
 
+#include <minix/bitmap.h>
 #include <minix/drivers.h>
+
+#define DEBUG 1
 
 int test_conf(void) {
     int res = 0;
@@ -15,7 +18,7 @@ int test_conf(void) {
         return res;
     }
     
-    printfd("DEBUG: RTC_REG_B : 0x%02X\n", regB);
+    printf("DEBUG: RTC_REG_B : 0x%02X\n", regB);
     
     printf("SET:%d PIE:%d AIE:%d UIE:%d SQWE:%d DM:%d MODE:%s DSE:%d\n",
         !!(bit_isset(regB, RTC_SET_BIT)),
@@ -31,7 +34,88 @@ int test_conf(void) {
 }
 
 int test_date(void) {
-	/* To be completed */
+	int res = 0;
+    unsigned long regA = 0;
+    unsigned long seconds, minutes, hours, day_of_month, month, year, day_of_week;
+    
+    do {
+        
+    sys_outb(RTC_ADDR_REG, RTC_REG_A);
+    res = sys_inb(RTC_DATA_REG, &regA);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_REG_A : 0x%X\n", regA);
+    
+    } while (bit_isset(regA, RTC_UIP_BIT));
+    
+    sys_outb(RTC_ADDR_REG, RTC_SECONDS);
+    res = sys_inb(RTC_DATA_REG, &seconds);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_SECONDS : 0x%X\n", seconds);
+    
+    sys_outb(RTC_ADDR_REG, RTC_MINUTES);
+    res = sys_inb(RTC_DATA_REG, &minutes);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_MINUTES : 0x%X\n", minutes);
+    
+    sys_outb(RTC_ADDR_REG, RTC_HOURS);
+    res = sys_inb(RTC_DATA_REG, &hours);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_HOURS : 0x%X\n", hours);
+    
+    sys_outb(RTC_ADDR_REG, RTC_DAY_OF_MONTH);
+    res = sys_inb(RTC_DATA_REG, &day_of_month);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_DAY_OF_MONTH : 0x%X\n", day_of_month);
+    
+    sys_outb(RTC_ADDR_REG, RTC_MONTH);
+    res = sys_inb(RTC_DATA_REG, &month);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_MONTH : 0x%X\n", month);
+    
+    sys_outb(RTC_ADDR_REG, RTC_YEAR);
+    res = sys_inb(RTC_DATA_REG, &year);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_YEAR : 0x%X\n", year);
+    
+    sys_outb(RTC_ADDR_REG, RTC_DAY_OF_WEEK);
+    res = sys_inb(RTC_DATA_REG, &day_of_week);
+    if (res != 0) {
+        printf("test_date: sys_inb failed.\n");
+        return res;
+    }
+    
+    printf("DEBUG: RTC_DAY_OF_WEEK : 0x%X\n", day_of_week);
+    
+    
+    return res;
 }
 
 int test_int(unsigned long duration) { 
