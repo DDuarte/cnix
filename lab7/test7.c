8bd9ede519e04586fbf6844b44d4eb07c0927c8f
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-11-12 17:41:11 +0000

    lab7: correct bugs.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@87 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- lab7/test7.c
+++ lab7/test7.c
@@ -1,7 +1,7 @@
 #include "test7.h"
 #include "uart.h"
 #include <stdio.h>
-
+#include <minix/bitmap.h>
 int test_conf(unsigned short base_addr) {
     uart_config_t conf;
 
@@ -25,13 +25,14 @@ int test_conf(unsigned short base_addr) {
 
 int test_set(unsigned short base_addr, unsigned long bits, unsigned long stop,
 	     long parity, unsigned long rate) {
+    static uart_parity_t parities[] = { UART_NO_PARITY, UART_EVEN_PARITY, UART_ODD_PARITY };    
     uart_config_t conf;
 
     /* uart_get_config(base_addr, &conf); */
 
     conf.bitRate = rate;
     conf.bits = bits;
-    conf.parity = parity;
+    conf.parity = parities[parity+1];
     conf.stop = stop;
 
     if (uart_set_config(base_addr, conf) != 0) {
@@ -41,18 +42,18 @@ int test_set(unsigned short base_addr, unsigned long bits, unsigned long stop,
     return 0;
 }
 
-int test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, unsigned long stop, long parity, unsigned long rate, int stringc, char *strings[]) {
+int test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, unsigned long stop, long parity, unsigned long rate, int stringc, char *strings[]) {    
     static uart_parity_t parities[] = { UART_NO_PARITY, UART_EVEN_PARITY, UART_ODD_PARITY };    
     uart_config_t prevConfig, newConfig;
-    unsigned long lsr, chr, previer;
+    unsigned long lsr, chr, previer, prevfcr;
     unsigned int i, j, strLength;
     
-    uart_get_config(base_addr, &prevConfing);
+    uart_get_config(base_addr, &prevConfig);
     newConfig = prevConfig;
     
     newConfig.bits = bits;
-    newconfig.stop = stop;
-    newConfig.bitrate = rate;
+    newConfig.stop = stop;
+    newConfig.bitRate = rate;
     newConfig.parity = parities[parity-1];
     
     
@@ -65,7 +66,7 @@ int test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, un
         do {
             uart_read(base_addr, UART_LSR_REG, &lsr);
             
-            if (bit_isset(lsr, LSR_RECEIVER_READY_BIT) {
+            if (bit_isset(lsr, LSR_RECEIVER_READY_BIT)) {
                 uart_read(base_addr, UART_RBR_REG, &chr);
                 printf("%c", (char)chr);
             }
@@ -74,7 +75,7 @@ int test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, un
         for (i = 0; i < stringc; i++) {
             strLength = strlen(strings[i]);
             for (j = 0; j < strLength; j++) {
-                uart_write(base_addr, UART_RBR_REG, (unsigned long)strings[i][j];
+                uart_write(base_addr, UART_RBR_REG, (unsigned long)strings[i][j]);
             }
             uart_write(base_addr, UART_RBR_REG, (unsigned long)' ');
         }
