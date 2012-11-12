a5e280a22306bea32df789f2bef6590ecaff7b19
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-11-12 18:36:14 +0000

    lab7: try to correct bugs again.
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@90 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- lab7/test7.c
+++ lab7/test7.c
@@ -42,6 +42,14 @@ int test_set(unsigned short base_addr, unsigned long bits, unsigned long stop,
     return 0;
 }
 
+void wait_to_be_ready(unsigned short base_addr) {
+    unsigned long lsr;
+    do {
+        uart_read(base_addr, UART_LSR_REG, &lsr);
+        tickdelay(20);
+    } while(!bit_isset(lsr, LSR_TRANSMITTER_HOLDING_REGISTER_EMPTY_BIT));
+}
+
 int test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, unsigned long stop, long parity, unsigned long rate, int stringc, char *strings[]) {    
     static uart_parity_t parities[] = { UART_NO_PARITY, UART_EVEN_PARITY, UART_ODD_PARITY };    
     uart_config_t prevConfig, newConfig;
@@ -60,28 +68,37 @@ int test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, un
     uart_read(base_addr, UART_IER_REG, &previer);
     uart_write(base_addr, UART_IER_REG, 0);
     
+    uart_read(base_addr, UART_FCR_REG, &prevfcr);
+    uart_write(base_addr, UART_FCR_REG, prevfcr & ~FCR_ENABLE_FIFO_BIT);
+    
     uart_set_config(base_addr, newConfig);
     
-    if (tx == 0) { 
+    
+    if (tx == 0) { /* a receber */
+    
         do {
-            uart_read(base_addr, UART_LSR_REG, &lsr);
-            
-            if (bit_isset(lsr, LSR_RECEIVER_READY_BIT)) {
-                uart_read(base_addr, UART_RBR_REG, &chr);
-                printf("%c", (char)chr);
-            }
+            wait_to_be_ready(base_addr);
+            uart_read(base_addr, UART_RBR_REG, &chr);
+            printf("%c", (char)chr);            
         } while((char)chr != '.');
+        printf("\n");
+        
     } else {
         for (i = 0; i < stringc; i++) {
             strLength = strlen(strings[i]);
             for (j = 0; j < strLength; j++) {
+                wait_to_be_ready(base_addr);
                 uart_write(base_addr, UART_RBR_REG, (unsigned long)strings[i][j]);
             }
+            wait_to_be_ready(base_addr);
             uart_write(base_addr, UART_RBR_REG, (unsigned long)' ');
         }
+        wait_to_be_ready(base_addr);
+        uart_write(base_addr, UART_RBR_REG, (unsigned long)'.');
     }
     
     uart_write(base_addr, UART_IER_REG, previer);
+    uart_write(base_addr, UART_FCR_REG, prevfcr);
     uart_set_config(base_addr, prevConfig);
     
     return 0;
