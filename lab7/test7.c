92d4bd7316ce4fc1adc39970dd83b6413e38773b
ei11099 <ei11099@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-11-12 17:37:51 +0000

    lab7: start implement of test_poll().
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@86 dd1ae04f-8964-4e22-877d-0b2b87089ac8

--- lab7/test7.c
+++ lab7/test7.c
@@ -42,8 +42,48 @@ int test_set(unsigned short base_addr, unsigned long bits, unsigned long stop,
 }
 
 int test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, unsigned long stop, long parity, unsigned long rate, int stringc, char *strings[]) {
-    printf("DEBUG: NOT IMPLEMENTED.\n");
-    return 1;
+    static uart_parity_t parities[] = { UART_NO_PARITY, UART_EVEN_PARITY, UART_ODD_PARITY };    
+    uart_config_t prevConfig, newConfig;
+    unsigned long lsr, chr, previer;
+    unsigned int i, j, strLength;
+    
+    uart_get_config(base_addr, &prevConfing);
+    newConfig = prevConfig;
+    
+    newConfig.bits = bits;
+    newconfig.stop = stop;
+    newConfig.bitrate = rate;
+    newConfig.parity = parities[parity-1];
+    
+    
+    uart_read(base_addr, UART_IER_REG, &previer);
+    uart_write(base_addr, UART_IER_REG, 0);
+    
+    uart_set_config(base_addr, newConfig);
+    
+    if (tx == 0) { 
+        do {
+            uart_read(base_addr, UART_LSR_REG, &lsr);
+            
+            if (bit_isset(lsr, LSR_RECEIVER_READY_BIT) {
+                uart_read(base_addr, UART_RBR_REG, &chr);
+                printf("%c", (char)chr);
+            }
+        } while((char)chr != '.');
+    } else {
+        for (i = 0; i < stringc; i++) {
+            strLength = strlen(strings[i]);
+            for (j = 0; j < strLength; j++) {
+                uart_write(base_addr, UART_RBR_REG, (unsigned long)strings[i][j];
+            }
+            uart_write(base_addr, UART_RBR_REG, (unsigned long)' ');
+        }
+    }
+    
+    uart_write(base_addr, UART_IER_REG, previer);
+    uart_set_config(base_addr, prevConfig);
+    
+    return 0;
 }
 
 int test_fifo(unsigned short base_addr, unsigned char tx, unsigned long bits, unsigned long stop, long parity, unsigned long rate, int stringc, char *strings[]) {
