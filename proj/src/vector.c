1c65387138cfbab7ebbf03e5ebc177ea48a95f59
ei11101 <ei11101@dd1ae04f-8964-4e22-877d-0b2b87089ac8>
2012-12-29 17:52:50 +0000

    proj: implement vector struct
    git-svn-id: https://svn.fe.up.pt/repos/lcom1213-t1g5@159 dd1ae04f-8964-4e22-877d-0b2b87089ac8

---
+++ proj/src/vector.c
@@ -0,0 +1,85 @@
+#include "vector.h"
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define DEFAULT_CAPACITY 10
+#define CAPACITY_RATE 1.5f
+
+void vector_new(vector* v) {
+    assert(v);
+
+    v->count = 0;
+    v->capacity = 0;
+    v->buffer = NULL;
+}
+
+void vector_free(vector* v) {
+    assert(v);
+
+    free(v->buffer);
+}
+
+int vector_size(vector* v) {
+    assert(v);
+
+    return v->count;
+}
+
+int vector_capacity(vector* v) {
+    assert(v);
+
+    return v->capacity;
+}
+
+void vector_push_back(vector* v, void* data) {
+    assert(v);
+    assert(data);
+
+    if (!v->capacity) {
+        v->capacity = DEFAULT_CAPACITY;
+        v->buffer = (void**)malloc(v->capacity * sizeof(void*));
+        memset(v->buffer, 0, v->capacity * sizeof(void));
+    }
+
+    if (v->capacity == v->count) {
+        v->capacity *= CAPACITY_RATE;
+        v->buffer = realloc(v->buffer, v->capacity * sizeof(void*));
+    }
+
+    v->buffer[v->count] = data;
+    v->count++;
+}
+
+void* vector_get(vector* v, int index) {
+    assert(v);
+    assert(index >= 0);
+    assert(index < v->count);
+
+    return v->buffer[index];
+}
+
+void vector_erase(vector* v, int index) {
+    assert(v);
+    assert(index >= 0);
+    assert(index < v->count);
+
+    v->buffer[index] = NULL; // maybe need a custom deallocator
+
+    int i, j;
+
+    void** arr = (void**)malloc(v->count * CAPACITY_RATE * sizeof(void*));
+    for (i = 0, j = 0; i < v->count; ++i) {
+        if (v->buffer[i]) {
+            arr[j] = v->buffer[i];
+            j++;
+        }
+    }
+
+    v->capacity = v->count * CAPACITY_RATE;
+
+    free(v->buffer);
+    v->buffer = arr;
+    v->count--;
+}
---
+++ proj/src/vector.h
@@ -0,0 +1,18 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct {
+    void** buffer; //> Data
+    int capacity; //> Allocated size of elements
+    int count; //> Number of elements
+} vector;
+
+void vector_new(vector* v); //> Create new vector
+void vector_free(vector* v); //> Delete vector
+int vector_size(vector* v); //> Size of vector
+int vector_capacity(vector* v); // Capacity of vector
+void vector_push_back(vector* v, void* data); //> Adds element to back of vector
+void* vector_get(vector* v, int index); //> Element at position #index
+void vector_erase(vector* v, int index); //> Removes element at position #index
+
+#endif // VECTOR_H
